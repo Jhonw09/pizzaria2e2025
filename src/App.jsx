72f605866f3b667049d87e2efa4c2a0f312f00bc
Jhonw09 <rm95197@estudante.fieb.edu.br>
2025-04-11 14:08:56 +0000

    imagem poke

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,63 @@
 import { useState } from "react"
- 
+import poke from './assets/pokeapi_256.png'
+
 function App(){
- 
+
   const [nome, setNome] = useState('JONAS')
-  const [idade, setIdade] = useState(16)
-  const [time, setTime] = useState('Corinthianhs')
+  const [idade, setIdade] = useState(18)
+  const [time, setTime] = useState('Corinthians')
   const [pi] = useState(3.14)
- 
+
   //var idade = 18;
   //let time = 'Corinthians';
   //const pi = 3.14;
- 
-  const Formulario = (props)=> {
-    const [valoe, setValor] = useState()
-   
+
+  const Formulario = (props) => {
+    const [valor, setValor] = useState()
+    
     return (
       <div>
-        <input
+        <input 
           className = "nome"
           placeholder= {props.sombra}
-          onChange={(e)=>{setNome(e.target.value)}}
+          onChange={(e)=>{setValor(e.target.value)}}
           type="text" />
- 
-        <button
+
+        <button 
           className="botao"
-          onClick= {()=>{
-            if(setValor.length > 0)
-              alert(valor)
-          }
+          onClick={()=>{
+            if(valor.length > 0){
+              alert(valor);
+            }
             else{
-              
-          alert={()=>{alert(props.nome + ' sua idade é ' + props.idade)}}    
-}}  
+              alert(props.nome + ' idade é ' + props.idade);
+            }
+            }}      
         >
           CLIQUE AQUI
         </button>
       </div>
     )
   }
- 
- 
+
+
   return (
     <div>
       <h3>Pizzaria 2E</h3>
- 
+      <img src= {poke} style={{width:330}}/>
 
-
-       <Formulario nome= "JONAS"
-       idade= {16}
-       sombra= "Digite nome.."/>
-
-      <Formulario  nome= "JONAS" sombra= "Digite nome..." />
-      <Formulario  nome= "KARINE" sombra= "Digite sua idade..."/>
-      <Formulario  nome= "JONAS" sombra= "Digite seu time..."/>
-      <Formulario  nome= "KARINE" sombra= "Dvalor de pi" />
- 
+      <Formulario nome="Jonas"
+          idade={45}
+          sombra="Digite nome..." />
+      <Formulario nome="Jonas" sombra="Digite sua idade..."/>
+      <Formulario nome="Karine" sombra="Digite seu time..." />
+      <Formulario nome="Karine" sombra="valor de pi" />
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
     </div>
   )
 }
- 
+
 export default App
- 
